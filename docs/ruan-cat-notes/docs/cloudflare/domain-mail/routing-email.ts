ddcc2d0686c9169a8ea52c4f59dfcf336da6f1e5
ruan-cat <1219043956@qq.com>
2026-03-17 01:33:39 +0800

    🐳 chore(notes)!: 记录cloudflare路由和AI平台注册的进度。

--- docs/ruan-cat-notes/docs/cloudflare/domain-mail/routing-email.ts
+++ docs/ruan-cat-notes/docs/cloudflare/domain-mail/routing-email.ts
@@ -7,18 +7,37 @@ interface platform {
 
 interface EmailItem {
 	email: string;
+	order: number;
 }
 
 type RutingEmail = EmailItem & Partial<platform>;
 
 export const routingEmails: RutingEmail[] = [
 	{
+		order: 1,
 		email: "use-kiro-001@ruan-cat.com",
 		openai: "已封号",
-		cursor: "试用期已过期",
+		cursor: "试用期已过期。已删除账号。",
 	},
 	{
+		order: 2,
 		email: "use-kiro-002@ruan-cat.com",
 		openai: "已封号",
+		// Your payment method is not eligible for a free trial. You can still upgrade to a paid plan and continue using Cursor.
+		cursor: "无法验证支付信息。已删除账号。",
+	},
+	{
+		order: 3,
+		email: "use-kiro-003@ruan-cat.com",
+		// 无法注册，在美区ip且指纹浏览器内，卡在手机号验证环节，无法注册账号。
+		// https://zhuanlan.zhihu.com/p/1944129840216273435
+		// 谷歌浏览器 无痕模式 先美区进入，验证手机号时，在直接退出，以中国ip输入手机号并接受验证码。即可注册成功。
+		cursor: "已注册。",
+	},
+	// use-cursor09-20260316@ruan-cat.com
+	{
+		order: 9,
+		email: "use-cursor09-20260316@ruan-cat.com",
+		openai: "",
 	},
 ];
